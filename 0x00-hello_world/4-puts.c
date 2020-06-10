29f5b63a1699c754ee58f170eb0f626f006691ba
quickcode20 <2021@holbertonschool.com>
2020-06-10 23:51:20 +0000

    Print exactly Programming is like bulding a multilingual

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string using put
+ * Return:  0
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
