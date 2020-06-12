7cacbe8c5b32a4e502b489d58c3b894c12edac86
quickcode20 <2021@holbertonschool.com>
2020-06-12 03:53:57 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - 2-print_alphabet.c
+*Return: 0
+*/
+int main(void)
+{
+	char x;
+
+	for (x = '0' ; x <= '9' ; ++x)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
