ed26e5fd9922ccecca92a186f8beeee3b7a3f21d
quickcode20 <2021@holbertonschool.com>
2020-06-12 04:07:44 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - 2-print_alphabet.c
+*Return: 0
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 'z' ; x >= 'a' ; x--)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
