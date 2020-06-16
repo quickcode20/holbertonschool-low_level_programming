856d76a20affe159c04dda51df607afa8935b151
quickcode20 <2021@holbertonschool.com>
2020-06-16 23:49:00 +0000

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+*jack_bauer - prints every minute of the day
+*Return: return nothing
+*/
+void jack_bauer(void)
+{
+	int min;
+
+	for (min = 0; min < 24 * 60; min++)
+	{
+		_putchar('0' + (min / 60) / 10);
+		_putchar('0' + (min / 60) % 10);
+		_putchar(':');
+		_putchar('0' + (min / 60) / 10);
+		_putchar('0' + (min / 60) % 10);
+		_putchar('\n');
+	}
+}
