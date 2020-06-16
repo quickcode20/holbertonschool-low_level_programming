79e162b3b206d9dc09903e3fafbd9f38e9bb99db
quickcode20 <2021@holbertonschool.com>
2020-06-16 17:05:55 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*main - Print alphabet in lowercase
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	char x;
+	for (x = 'a' ; x <= 'z' ; ++x)
+	{
+		_putchar(x);
+	}
+		_putchar('\n');
+}
+
