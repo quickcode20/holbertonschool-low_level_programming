b5205274db20b494e6463a95993a320bc31eabb3
quickcode20 <2021@holbertonschool.com>
2020-06-16 21:22:56 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*print_sign - prints the sign of a number +, -, 0
+*@n: input number
+*Return: 1 positive, 0 zero, 1 otherwise
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + '0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
