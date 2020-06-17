fc00bba0d68fb862ae0d6e67cbec50f8b24115a9
quickcode20 <2021@holbertonschool.com>
2020-06-17 00:27:55 +0000

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+*add - adds two integers and returns the result
+*@x: integer 1
+*@y: integer 2
+*Return: result of the sum of the two integers
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
+
