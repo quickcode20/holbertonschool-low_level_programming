79a1770003b7e60192074d8ddfd4f74325bd70ec
quickcode20 <2021@holbertonschool.com>
2020-06-18 15:36:07 +0000

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*_isdigit - checks for a digit (0 through 9)
+*@c: given input
+*Return: returns 1 if c is a digit 0 otherwise
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
