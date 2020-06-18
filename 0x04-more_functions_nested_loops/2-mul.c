6b1c970399bf92e0506e08706b86ba6fff177415
quickcode20 <2021@holbertonschool.com>
2020-06-18 15:54:06 +0000

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+*mul - multiplies two integers
+*@a: input first integer
+*@b: input second integer
+*Return: the multiplication result between a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
