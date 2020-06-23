c75f18fe9797fe934dadffe498d14cefd94548a1
quickcode20 <quickcode20@aol.com>
2020-06-23 12:57:02 +0000

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+*print_rev - prints a string in reverse
+*@s: given value
+*return: 0;
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
