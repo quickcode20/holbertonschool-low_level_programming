d959ad937555f0250783758423a0d0ebeabf95ad
quickcode20 <quickcode20@aol.com>
2020-06-23 12:46:15 +0000

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_puts - prints a string followed by a new line
+*@str: given value
+*Return: nothing
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar ('\n');
+}
+
