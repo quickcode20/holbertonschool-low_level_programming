f28d52547d59c6e90ddaf9983fe861d5f844207e
quickcode20 <quickcode20@aol.com>
2020-06-23 16:22:01 +0000

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+*rev_string - reverses a string
+*@s: given value
+*Return: nothing
+*/
+void rev_string(char *s)
+{
+	int leng, count, str;
+	leng = 0;
+
+	while (s[leng + 1] != 0)
+	{
+		leng++;
+	}
+	for (count = 0; count <= leng /2; count++)
+	{
+		str = s[count];
+		s[count] = s[leng - count];
+		s[leng -count] = str;
+	}
+}
+
