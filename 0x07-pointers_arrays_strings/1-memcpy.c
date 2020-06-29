b1ca6545e7db045e44341bb3da24feb60393c0df
quickcode20 <quickcode20@aol.com>
2020-06-29 01:02:32 +0000

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*_memcpy - copies memory area
+*@n: bytes from memory area
+*@src: source memory area
+*@dest: destination for copy
+*Return: pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src [i];
+	}
+	return (dest);
+}
+
